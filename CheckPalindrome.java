872bf5cc2a35068a3251f6fa721df9529bfb58ef
22K61A4235 <143991523+22K61A4235@users.noreply.github.com>
2023-09-19 09:41:57 +0530

    CheckPalindrome.java

---
+++ CheckPalindrome.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class CheckPlindrome{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a positive integer check it is palindrome are not: ");
+        int n = sc.nextInt();
+        int temp = n;
+        int rev = 0;
+        while(n>0){
+            int rem = n%10;
+            rev = (rev*10)+rem;
+            n = n/10;
+        }
+        if(temp==rev){
+            System.out.println(temp+"is a palindrome");
+        }
+        else
+        {
+            System.out.println(temp+" is not a palindrome");
+        }
+    }
+}
